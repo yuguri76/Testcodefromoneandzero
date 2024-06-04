1e2ddf1bff82fb0e1ee585ff1db7bf1ba3231f1d
dami0806 <dami0804@naver.com>
2024-06-04 13:05:47 +0900

    init

---
+++ src/main/java/com/sparta/oneandzerobest/OneAndZeroBestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.oneandzerobest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneAndZeroBestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneAndZeroBestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/oneandzerobest/OneAndZeroBestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.oneandzerobest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneAndZeroBestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
