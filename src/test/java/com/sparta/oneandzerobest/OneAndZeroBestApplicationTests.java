e9c422a9f9dc48f63a9d5f25329b6e0f107b36e0
joon <tjrwns3428@gmail.com>
2024-06-04 17:23:13 +0900

    feat : TimeStamp 클래스 추가, 프로젝트 시작점(main) @EnableJpaAuditin 추가

--- src/test/java/com/sparta/oneandzerobest/OneAndZeroBestApplicationTests.java
+++ src/test/java/com/sparta/oneandzerobest/OneAndZeroBestApplicationTests.java
@@ -2,7 +2,9 @@ package com.sparta.oneandzerobest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootTest
 class OneAndZeroBestApplicationTests {
 
