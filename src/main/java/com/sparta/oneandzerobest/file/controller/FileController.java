fdb4100ce401c2e57564d5f8bb6fd401e557ec36
chamcham0707 <cheamin6306@naver.com>
2024-06-04 20:24:44 +0900

    S3Config 추가, S3UploadService 추가, build.gradle 추가, application.yml 추가 - 아직 미완성 일단 보류 단계

---
+++ src/main/java/com/sparta/oneandzerobest/file/controller/FileController.java
@@ -0,0 +1,14 @@
+package com.sparta.oneandzerobest.file.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+    @PostMapping()
+    public ResponseEntity<String> saveFile()
+}
